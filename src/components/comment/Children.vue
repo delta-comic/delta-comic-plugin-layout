ba88b7ecaa927b8e3dfd4e48bc20323e9063453b
wenxig <wenxinguo12@gmail.com>
2026-02-20 00:50:02 +0800

    fix: [pub] import sender name

--- src/components/comment/Children.vue
+++ src/components/comment/Children.vue
@@ -2,7 +2,7 @@
 import { uni } from '@delta-comic/model'
 import { DcPopup, DcWaterfall } from '@delta-comic/ui'
 import { computed, shallowRef } from 'vue'
-import Sender from './sender.vue'
+import Sender from './Sender.vue'
 const $props = defineProps<{ item: uni.item.Item }>()
 const parentComment = shallowRef<uni.comment.Comment>()
 
@@ -18,12 +18,31 @@ const CommentRow = computed(() => uni.comment.Comment.commentRow.get($props.item
 </script>
 
 <template>
-  <DcPopup v-model:show="isShowPopup" position="bottom" lock-scroll ref="floatPopup" :overlay="false"
-    class="**:overflow-x-hidden h-[70vh]">
-    <DcWaterfall :source="parentComment.children" :padding="0" :col="1" :gap="0" v-if="parentComment"
-      v-slot="{ item: comment }" class="h-[calc(100%-40px)]! bg-(--van-background)"
-      :data-processor="v => (parentComment ? [parentComment, ...v] : v)">
-      <component :is="CommentRow" :parentComment :comment :item @click-user="$emit('user', $event)" />
+  <DcPopup
+    v-model:show="isShowPopup"
+    position="bottom"
+    lock-scroll
+    ref="floatPopup"
+    :overlay="false"
+    class="h-[70vh] **:overflow-x-hidden"
+  >
+    <DcWaterfall
+      :source="parentComment.children"
+      :padding="0"
+      :col="1"
+      :gap="0"
+      v-if="parentComment"
+      v-slot="{ item: comment }"
+      class="h-[calc(100%-40px)]! bg-(--van-background)"
+      :data-processor="v => (parentComment ? [parentComment, ...v] : v)"
+    >
+      <component
+        :is="CommentRow"
+        :parentComment
+        :comment
+        :item
+        @click-user="$emit('user', $event)"
+      />
     </DcWaterfall>
     <Sender :item :aim="parentComment" v-if="parentComment" />
   </DcPopup>
