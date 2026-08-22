8573e34dd6512ffe9cfdeeff2def24b028f107ec
wenxig <wenxinguo12@gmail.com>
2026-08-22 15:29:10 +0800

    feat(app): 跟进版本

--- packages/app/test/manifest.test.ts
+++ packages/app/test/manifest.test.ts
@@ -12,7 +12,7 @@ describe('plugin manifest', () => {
       entry: { cssPath: 'src/index.css', jsPath: 'src/main.ts' },
       name: { id: 'layout' },
       require: [{ id: 'core' }],
-      version: { plugin: '1.2.3', supportCore: '>=3.0.0-next.15 <4.0.0' },
+      version: { plugin: '1.2.3', supportCore: '>=3.0.0-next.16 <4.0.0' },
     })
   })
 
--- script/artifacts.test.ts
+++ script/artifacts.test.ts
@@ -14,7 +14,7 @@ const manifest = {
   entry: { cssPath: 'index.css', jsPath: 'index.js' },
   name: { display: 'Layout', id: 'layout' },
   require: [{ id: 'core' }],
-  version: { plugin: '1.0.0-next.1', supportCore: '>=3.0.0-next.15 <4.0.0' },
+  version: { plugin: '1.0.0-next.1', supportCore: '>=3.0.0-next.16 <4.0.0' },
 }
 
 async function createFixture(archiveManifest = manifest, includeCss = true) {
